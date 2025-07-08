0e251298e2d108e9f14ef6be5f7419a04bfa55f0
michael williams <williamsmikeyj@outlook.com>
2025-07-07 21:12:40 -0400

    new model promp coder
    Signed-off-by: michael williams <williamsmikeyj@outlook.com>

---
+++ strains.py
@@ -0,0 +1,35 @@
+# venv\Scripts\activate.bat
+
+
+
+strains = {
+    'riddler' : {
+    "strain": "Riddler",
+    "date": "02/06/2024",
+    "form": 'Smoking',
+    'dominance' : 'Hybrid',
+    'gram_size' : 3.5,
+    'details' : 'Flower',
+    'brand' : 'Trulieve',
+    'price' : 23.00,
+    'terpenese' : ['(R)-(+)-Limonene', 'beta-Pinene', 'Trans-Caryophyllene'],
+    'strain_number' : 66899,
+    },
+    'cherry tart': {
+        '66899_0005290868': {
+            'batch': 000,
+            'date': '02/05/2024',
+            "form": 'Smoking',
+            'dominance' : 'Hybrid',
+            'gram_size' : 3.5,
+            'details' : 'Flower',
+            'brand' : 'Trulieve',
+            'price' : 23.00,
+            'terpenese' : ['(R)-(+)-Limonene', 'beta-Pinene', 'Trans-Caryophyllene'],
+            'strain_number' : 66899,
+        },
+        '66899_0005290878'
+        
+    }
+}
+
